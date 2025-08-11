816f21e8b3397d98924396334246da350b20bbd4
Quách Thành Long <stephensouth1307@gmail.com>
2025-08-11 09:07:48 +0700

    -update

--- Assets/Scripts/Background.cs
+++ Assets/Scripts/Background.cs
@@ -1,16 +1,8 @@
 using UnityEngine;
-
+using UnityEngine.UI;
+using UnityEngine.Audio;
 public class Background : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField] private AudioMixer myMixer;
+    [SerializeField] private Slider musicSlider;
 }
