0678fbcfc4d9e19f0f6d483df05abf86687a164b
Quách Thành Long <stephensouth1307@gmail.com>
2025-08-11 09:51:33 +0700

    -update

--- Assets/Scripts/VolumeSettingGame.cs
+++ Assets/Scripts/VolumeSettingGame.cs
@@ -1,8 +1,42 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
+
 public class VolumeSettingGame : MonoBehaviour
 {
     [SerializeField] private AudioMixer audioMixer;
     [SerializeField] private Slider musicSlider;
+    [SerializeField] private AudioSource musicSource;
+
+    private void Start()
+    {
+        // Lấy giá trị âm lượng đã lưu
+        if (PlayerPrefs.HasKey("MusicVolume"))
+        {
+            float savedVolume = PlayerPrefs.GetFloat("MusicVolume");
+            musicSlider.value = savedVolume;
+            SetMusicVolume(savedVolume);
+        }
+        else
+        {
+            musicSlider.value = 0.75f;
+            SetMusicVolume(0.75f);
+        }
+
+        // Bắt đầu phát nhạc
+        if (musicSource != null && !musicSource.isPlaying)
+            musicSource.Play();
+    }
+
+    public void OnMusicSliderChanged()
+    {
+        SetMusicVolume(musicSlider.value);
+        PlayerPrefs.SetFloat("MusicVolume", musicSlider.value);
+    }
+
+    private void SetMusicVolume(float volume)
+    {
+        // Chuyển từ 0-1 sang dB (-80 đến 0)
+        audioMixer.SetFloat("Music", Mathf.Log10(volume) * 20);
+    }
 }
